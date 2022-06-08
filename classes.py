4272b378f3a987a38d19c75f5b816b683c7cda36
ulas <u.c.demirtas@gmail.com>
2022-06-08 15:07:26 +0200

    new attributes to the classe

--- classes.py
+++ classes.py
@@ -1,4 +1,13 @@
-# Classes file
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------------
+# Created By  :
+# Created Date: 31-05-2022
+# ---------------------------------------------------------------------------
+"""
+"""
+import numpy as np
+from math import ceil
 
 
 class Student:
@@ -10,6 +19,7 @@ class Student:
         self._last_name = last_name
         self._studentnumber = student_num
         self._courses = courses
+        self._timetable = np.array([5, 5])  # hierin komt een tuple: (Room, Course)
 
 
 class Room:
@@ -17,22 +27,44 @@ class Room:
         self._roomnumber = roomnumber
         self._capacity = capacity
 
+        # hierin komt een tuple: (lijst[student_num], Course)
+        self._timetable = np.array([5, 5])
+
 
 class Course:
     def __init__(
         self,
-        vak,
-        hoorcollege,
-        werkcollege,
+        course,
+        lecture,
+        seminar,
         max_students_werk,
         practica,
         max_students_prac,
-        verwacht,
+        expected,
     ):
-        self._course = vak
-        self._lecture = hoorcollege
-        self._worklecture = werkcollege
-        self._max_students_work = max_students_werk
-        self._practica = practica
-        self._max_students_prac = max_students_prac
-        self._expected_students = verwacht
+        self._course_name = course
+        self._n_lecture = lecture
+        self._n_seminar = seminar
+        self._n_practica = practica
+        self._seminar_cap = max_students_werk
+        self._pract_cap = max_students_prac
+        self._expected_students = expected
+
+        # Calculate amount of classes needed for one seminar
+        self._class_per_seminar = ceil(self._expected_students / self._seminar_cap)
+
+        # Calculate total needed classes for all seminars
+        self._needed_seminars = self._n_seminar * self._class_per_seminar
+
+        # Calculate students per seminar
+        self._students_per_seminar = ceil(
+            self._expected_students / self._class_per_seminar
+        )
+
+        # TODO hoeveelheid studenten per practicum berekenen, net als hierboven ^
+
+    def get_expected_stud(self) -> None:
+        return self._expected_students
+
+    def set_expected_stud(self, value) -> None:
+        self._expected_students = value
